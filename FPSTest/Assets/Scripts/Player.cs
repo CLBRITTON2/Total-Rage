1fa267e7de3a83c9081c9c68e22e116ef28b8c37
Christopher Britton <108378515+CLBRITTON2@users.noreply.github.com>
2022-08-09 19:46:25 -0400

    Implement player movement

--- FPSTest/Assets/Scripts/Player.cs
+++ FPSTest/Assets/Scripts/Player.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public double speed = 10;
+    public CharacterController playerController;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +15,6 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        playerController.Move(Vector3.forward);
     }
 }
